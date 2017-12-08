c5c87aa9f84c6215fadb24827276fbf0d68f947b
Stas <mail@k1on.ru>
2017-12-08 18:10:27 +0300

    * Added "!!!" * Test

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron'
 const {appUpdater} = require('./autoupdater');
+const isDev = require('electron-is-dev');
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
